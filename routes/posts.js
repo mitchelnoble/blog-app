e6953c2afb44ca1f0a3777abebb43dd46a68f4c2
Evyn Newton <newton.evyn@gmail.com>
2020-12-18 16:41:09 -0500

    added a home route, and updating scripts

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,16 @@
 //API routes
-const { Router } = require('express')
-const controllers = require('../controllers/posts')
+const { Router } = require("express");
+const controllers = require("../controllers/posts");
 
-const router = Router()
+const router = Router();
 
-router.get('/posts', controllers.getPosts)
-router.get('/posts/:id', controllers.getPost)
-router.post('/posts', controllers.createPost)
-router.put('/posts/:id', controllers.updatePost)
-router.delete('/posts/:id', controllers.deletePost)
+router.get("/", (req, res) => {
+  res.send("I am root.");
+});
+router.get("/posts", controllers.getPosts);
+router.get("/posts/:id", controllers.getPost);
+router.post("/posts", controllers.createPost);
+router.put("/posts/:id", controllers.updatePost);
+router.delete("/posts/:id", controllers.deletePost);
 
-module.exports = router
+module.exports = router;
