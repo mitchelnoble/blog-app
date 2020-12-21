ea2f9bf383b3d3da6ce98bd245bd26676f99f43b
Liz Yrineo <lizyrineo@Lizs-MacBook-Pro.local>
2020-12-21 14:30:49 -0700

    addCard complete

--- client/src/screens/AddScreen/AddCard.jsx
+++ client/src/screens/AddScreen/AddCard.jsx
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import post from '../../../../models/post';
+import { createPost } from '../services/posts'
 
-const AddCard = () => {
+
+const AddCard = (props) => {
+  const history = useHistory()
+  const [post, setPost] = useState({
+      author: '',  
+      title: '',
+      imgUrl: '',
+      content: ''
+  })
+  
+  const [isCreated, setIsCreated] = useState(false)
+
+  const handleChange= (e) => {
+    const { name, value} = e.target
+      setPost({
+        ...post,
+        [name]: value
+    })
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const isCreated = await addCard(isCreated)
+    setIsCreated({ isCreated })
+    history.push('/')
+}
+ 
+  if (isCreated) {
+    return <Redirect to={``}/>
+  }
+
+  if (params.id) {
+    api.updatePosts(params.id, fields)
+    props.setToggleFetch((prev) => !prev);
+    history.push("/");
+  } else {
+    history.push("/");
+  }
+}
+
   return (
     <div>
-      
+      <form className='addForm' onSubmit={handleSubmit}>
+        <input
+          className='input-name'
+          placeholder='Home'
+          value={post.author}
+          name='author'
+          required
+          autoFocus
+          onChange={handleChange}
+        />
+        <input 
+          className='input-title'
+          placeholder='title'
+          value={post.title}
+          name='title'
+          onChange={handleChange}
+          />
+        < input
+          className='input-image'
+          placeholder='image'
+          value={post.img}
+          name='image'
+          onChange={handleChange}
+        />
+        <input 
+          className='input-content'
+          placeholder='content'
+          value={post.content}
+          name='content'
+          onChange={handleChange}
+        />
+        <button type='submit' className='submit-button'>Bazinga</button>
+      </form>
     </div>
   );
 };
---
+++ client/src/screens/PostMan/PostMan.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PostMan = () => {
+  return (
+    <div className='post-dude'>
+      <Link to='/'>
+
+      </Link>  
+    </div>
+  );
+};
+
+export default PostMan;
