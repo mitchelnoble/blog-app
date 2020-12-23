abe6f799068292260623b8fb7797eed8548a5ae4
Shelby /sexton <shelbysexton@Shelbys-MacBook-Air.local>
2020-12-22 23:08:40 -0500

    homepage done

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 
-export default function Header() {
-  return (
-    <div className="header">
-      <Link to="/">Best Blog App Ever.</Link>
-     <img className= "post-dude" src="https://i.ibb.co/CJX089X/Untitled-design-1-1.png" alt="Untitled-design-1-1" border="0"/>
-    </div>
-  );
-}
+// export default function Header() {
+//   return (
+//     <div className="header">
+//       <Link className="header-link" to="/">Best Blog App Ever.</Link>
+//      <img className= "post-dude" src="https://i.ibb.co/CJX089X/Untitled-design-1-1.png" alt="Untitled-design-1-1" border="0"/>
+//     </div>
+//   );
+// }
